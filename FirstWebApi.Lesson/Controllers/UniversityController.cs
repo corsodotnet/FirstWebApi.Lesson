49cf36911c830b732fcd04b37556f325ed9635d6
Bruno Ferreira <bruno_ferreira@hotmail.it>
2022-05-24 16:31:11 +0200

    Add project files.

---
+++ FirstWebApi.Lesson/Controllers/UniversityController.cs
@@ -0,0 +1,76 @@
+﻿using FirstWebApi.Lesson.Database;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FirstWebApi.Lesson.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UniversityController : ControllerBase
+    {
+         private  UniversityContext _context;
+        public UniversityController(UniversityContext context)
+        {
+            _context = context;
+        }
+        // GET: api/<UniversityController>
+        [HttpGet]
+        public IEnumerable<Corso> Get()
+        {
+            Corso corso;
+            using(_context)
+            {
+                try
+                {
+                  return _context.Corso.ToList();
+                    
+                }
+                catch (System.Exception ex )
+                {
+                    throw;
+                }
+
+        }
+
+        // GET api/<UniversityController>/5
+        [HttpGet("{id}")]
+        public Corso Get(string Title)
+        {
+            Corso corso;
+            corso = _context.Corso
+                     .Where(c => c.Name == Title).First();
+            var data = _context.Corso
+                .Include(s => s.Students)
+                .First(x => x.Id == corso.Id);
+
+
+            return corso;
+        }
+
+        // POST api/<UniversityController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+           
+
+        }
+
+        // PUT api/<UniversityController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+            _context.Update();
+        }
+
+        // DELETE api/<UniversityController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _context.Remove();
+        }
+    }
+}
---
+++ FirstWebApi.Lesson/Database/UniversityContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+
+namespace FirstWebApi.Lesson.Database
+{
+    public class UniversityContext : DbContext
+    {
+        public UniversityContext(DbContextOptions<UniversityContext> opts):base(opts)
+        {
+        }
+        public UniversityContext()
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // optionsBuilder.UseSqlServer(ConfigurationManager.ConnectionStrings["BlogDbContextDatabase"].ConnectionString);
+            optionsBuilder.UseSqlServer(@"Server=localhost;Database=UniversityDb; User id = sa ; password = Pass@word01;Trusted_Connection=False");
+        }
+
+        public DbSet<Corso> Corso { get; set; }
+        public DbSet<Studente> Studente { get; set; }
+    }
+    public class Studente
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public DateTime DatePublished { get; set; }
+        public virtual Corso? Corso { get; set; }
+    }
+    public class Corso
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public DateTime DatePublished { get; set; }
+        public virtual List<Studente>? Students { get; set; }
+    }
+}
